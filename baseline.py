3f977ad84cc505d7a3133e3807abcbeb617278c7
ytyz1307zzh <31958914+ytyz1307zzh@users.noreply.github.com>
2018-05-18 00:26:52 +0800

    网上的baseline代码+本地100000训练集

---
+++ baseline.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+from scipy import sparse
+
+dir_path=r'C:/Users/Zhihan Zhang/Desktop/算分/preliminary_contest_data/preliminary_contest_data'
+
+ad_feature=pd.read_csv(dir_path+r'/adFeature.csv')
+user_feature=pd.read_csv(dir_path+r'/userFeature_tiny_100000.csv')
+train=pd.read_csv(dir_path+r'/train_100000.csv')
+
+train=pd.merge(train,ad_feature,on='aid',how='left')
+train=pd.merge(train,user_feature,on='uid',how='left')
+train=train.fillna('-1')
+
+one_hot_feature=['LBS','age','carrier','consumptionAbility','education','gender','house','os','ct','marriageStatus','advertiserId','campaignId', 'creativeId',
+       'adCategoryId', 'productId', 'productType']
+vector_feature=['appIdAction','appIdInstall','interest1','interest2','interest3','interest4','interest5','kw1','kw2','kw3','topic1','topic2','topic3']
+for feature in one_hot_feature:
+    try:
+        train[feature] = LabelEncoder().fit_transform(train[feature].apply(int))
+    except:
+        train[feature] = LabelEncoder().fit_transform(train[feature])
+
+train_y=train.pop('label')
+train, test, train_y, test_y = train_test_split(train,train_y,test_size=0.2, random_state=2018)
+enc = OneHotEncoder()
+train_x=train[['creativeSize']]
+test_x=test[['creativeSize']]
+
+for feature in one_hot_feature:
+    enc.fit(train[feature].values.reshape(-1, 1))
+    train_a=enc.transform(train[feature].values.reshape(-1, 1))
+    test_a = enc.transform(test[feature].values.reshape(-1, 1))
+    train_x= sparse.hstack((train_x, train_a))
+    test_x = sparse.hstack((test_x, test_a))
+print('one-hot prepared !')
+
+cv=CountVectorizer()
+for feature in vector_feature:
+    cv.fit(train[feature])
+    train_a = cv.transform(train[feature])
+    test_a = cv.transform(test[feature])
+    train_x = sparse.hstack((train_x, train_a))
+    test_x = sparse.hstack((test_x, test_a))
+print('cv prepared !')
+
+print(train_x.shape)
+
+def LGB_test(train_x,train_y,test_x,test_y):
+    print("LGB test")
+    clf = lgb.LGBMClassifier(
+        boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
+        max_depth=-1, n_estimators=1500, objective='binary',
+        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
+        learning_rate=0.05, min_child_weight=50, random_state=2018, n_jobs=-1
+    )
+    clf.fit(train_x, train_y,eval_set=[(train_x,train_y),(test_x,test_y)],eval_metric='auc',early_stopping_rounds=100)
+    return clf,clf.best_score_
+
+clf,best_score=LGB_test(train_x,train_y,test_x,test_y)
+
